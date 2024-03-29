7d7e7bd299208e43d2f25479074cdfdd74f7eb40
NXT77 <118898181+NXT77@users.noreply.github.com>
2024-03-29 09:49:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const { Client } = require('discord.js-selfbot-v13');
-const client = new Client({checkUpdate:false}); 
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus } = require('@discordjs/voice');
+const play = require('play-dl'); 
+require('dotenv').config();
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
 const express = require("express")
 const app = express();
 var listener = app.listen(process.env.PORT || 2000, function () {
@@ -12,9 +18,54 @@ app.get('/', (req, res) => {
   <center><h1>Bot 24H ON!</h1></center
   </body>`)
 });
+const channelId = '1210233988699131946';
+const guildId = '1011820688228765828'; 
+const Url = 'https://youtu.be/fLkdQeeRtYs?si=02zwuFdQbytcD09f'; 
 
-client.on('ready', async () => {
-  console.log(`${client.user.username} is Ready For Working 24/7!`);
-})
+client.on('ready', () => {
+  console.log(`✅ | Logged in as ${client.user.tag}`);
+  joinAndPlayQuran(guildId, channelId);
+});
 
-client.login(process.env.token);
+async function playQuran(channel, message) {
+  const player = createAudioPlayer();
+  const connection = joinVoiceChannel({
+    channelId: channel.id,
+    guildId: channel.guild.id,
+    adapterCreator: channel.guild.voiceAdapterCreator,
+  });
+
+  connection.subscribe(player);
+
+  const stream = await play.stream(Url);
+  const resource = createAudioResource(stream.stream, { inputType: stream.type });
+  player.play(resource);
+
+  player.on(AudioPlayerStatus.Playing, () => {
+    message.channel.send({ embeds: [new EmbedBuilder().setDescription('**تم تشغيل القرآن**').setColor(0x00ff00)] });
+  });
+
+  player.on('error', error => console.error(`Error: ${error.message}`));
+}
+
+async function joinAndPlayQuran(guildId, channelId) {
+  const channel = await client.channels.fetch(channelId);
+  const connection = joinVoiceChannel({
+    channelId: channel.id,
+    guildId: guildId,
+    adapterCreator: channel.guild.voiceAdapterCreator,
+  });
+
+  const player = createAudioPlayer();
+
+  const stream = await play.stream(Url);
+  const resource = createAudioResource(stream.stream, { inputType: stream.type });
+
+  player.play(resource);
+  connection.subscribe(player);
+
+  player.on(AudioPlayerStatus.Playing, () => console.log('Playing Quran'));
+  player.on('error', error => console.error(`Error: ${error.message}`));
+}
+
+client.login(process.env.TOKEN);
